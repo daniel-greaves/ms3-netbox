5341ca75f4a4eebdbb2e70f26019b6901231a575
Daniel Greaves <77550299+daniel-greaves@users.noreply.github.com>
2024-04-08 20:44:35 +0100

    Update b2b_automation.py

--- b2b_automation.py
+++ b2b_automation.py
@@ -10,7 +10,22 @@ class NewBranchScript(Script):
     class Meta:
         name = "New B2B Order"
         description = "Provision a new business connection"
-
+        fieldsets = (
+            ('Customer Information', ('wholesale_provider')),
+            ('Site Information', ('site_name', 'site_address')),
+            # ('Service Information', ('service_profile'))
+        )
+    
+    wholesale_provider = ObjectVar(
+        description = "The name of the reseller ordering this service",
+        model = Tenant,
+        query_params = {
+            'group': 'wholesale-providers'
+        }
+    )
+    site_name = StringVar(
+        description = "Name of the site or business"
+    )
     site_address = TextVar(
         description = "Name of the old location"
     )
