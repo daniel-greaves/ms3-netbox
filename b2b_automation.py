56425b7472686422a166ff73ff04437182618cfa
Daniel Greaves <77550299+daniel-greaves@users.noreply.github.com>
2024-04-08 20:27:03 +0100

    Update b2b_automation.py

--- b2b_automation.py
+++ b2b_automation.py
@@ -11,23 +11,28 @@ class NewBranchScript(Script):
         name = "New B2B Order"
         description = "Provision a new business connection"
         fieldsets = (
-            ('First group', ('site_name', 'site_name1')),
-            ('Second group', ('location_name', 'wholesale_provider')),
+            ('Customer Information', ('wholesale_provider')),
+            ('Site Information', ('site_name', 'site_address')),
+            ('Service Information', ('service_profile')),
         )
-    
-    site_name = StringVar(
-        description="Name of the old site"
-    )
-    location_name = StringVar(
-        description="Name of the old location"
-    )
-    site_name1 = StringVar(
-        description="Name of the new site"
-    )
+
     wholesale_provider = ObjectVar(
         description="The name of the reseller ordering this service",
         model=Tenant,
         query_params={
             'group': 'wholesale-providers'
         }
+    )
+    site_name = StringVar(
+        description="Name of the site or business"
+    )
+    site_address = TextVar(
+        description="Name of the old location"
+    )
+    service_profile = ChoiceVar(
+        choices = (
+            ('1g_eth', '1G Ethernet Layer 2'),
+            ('10g_eth', '10G Ethernet Layer 2'),
+            ('1g_fttp', '1G FTTP'),
+        )
     )
