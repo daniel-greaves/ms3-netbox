86617e552c74b68e2b24271349d88a8a1319949d
Daniel Greaves <77550299+daniel-greaves@users.noreply.github.com>
2024-04-08 15:09:53 +0100

    Update b2b_automation.py

--- b2b_automation.py
+++ b2b_automation.py
@@ -9,9 +9,8 @@ class NewBranchScript(Script):
     class Meta:
         name = "New B2B Order"
         description = "Provision a new business connection"
-    fieldsets = (
-        (('Site Information'), (site_name))
-    )
+        fieldsets = ((('Site Information'), ('site_name')))
+    
     site_name = StringVar(
         description="Name of the new site"
     )
