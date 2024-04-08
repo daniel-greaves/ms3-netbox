b7d00c899f05a46250de05b7af0c98c27ae57717
Daniel Greaves <77550299+daniel-greaves@users.noreply.github.com>
2024-04-08 15:07:16 +0100

    Update b2b_automation.py

--- b2b_automation.py
+++ b2b_automation.py
@@ -9,41 +9,10 @@ class NewBranchScript(Script):
     class Meta:
         name = "New B2B Order"
         description = "Provision a new business connection"
-
+    fieldsets = (
+        (('Site Information'), ('site_name'))
+    )
     site_name = StringVar(
         description="Name of the new site"
     )
-    switch_count = IntegerVar(
-        description="Number of access switches to create"
-    )
-    switch_model = ObjectVar(
-        description="Access switch model",
-        model=DeviceType
-    )
-    router_count = IntegerVar(
-        description="Number of routers to create"
-    )
-    router_model = ObjectVar(
-        description="Router model",
-        model=DeviceType
-    )
-    ap_count = IntegerVar(
-        description="Number of APs to create"
-    )
-    ap_model = ObjectVar(
-        description="AP model",
-        model=DeviceType,
-        query_params={
-            'manufacturer': 'adva'
-        }
-    )
-    ip_address = IPAddressWithMaskVar(
-        description="IP Address of ADVA"
-    )
-    server_count = IntegerVar(
-        description="Number of servers to create"
-    )
-    server_model = ObjectVar(
-        description="Server model",
-        model=DeviceType
-    )
+
