dc263c135432fcd34baa52f2bb65773799ccd5ae
Daniel Greaves <77550299+daniel-greaves@users.noreply.github.com>
2024-04-08 21:33:06 +0100

    Update b2b_automation.py

--- b2b_automation.py
+++ b2b_automation.py
@@ -66,5 +66,12 @@ class NewBranchScript(Script):
         
         if device_model:
             device_type = DeviceType.objects.get(model = device_model)
-            
+            nte = Device(
+                device_type = device_type
+                name = f'{site.slug.upper()}-NTE-1',
+                site = site,
+                status = DeviceStatusChoices.STATUS_PLANNED,
+                device_role = nte_role
+            )
+            nte.save()
             self.log_success(f"Created new NTE: {device_type}")
