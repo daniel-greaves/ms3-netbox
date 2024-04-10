f3d8ecf933e7002fc3832a450fb1648de14a4e39
Daniel Greaves <77550299+daniel-greaves@users.noreply.github.com>
2024-04-10 22:52:32 +0100

    Update email_report.py

--- email_report.py
+++ email_report.py
@@ -1,5 +1,9 @@
 class NewBusinessService(Script):
-	def run():
+	class Meta:
+	        name = "New B2B Order"
+	        description = "Provision a new business connection"
+	
+	def run(self, data, commit):
 		return requests.post(
 			"https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
 			auth=("api", "8813868bcb942415b4acd20143deb784-4b670513-efd3e03e"),
