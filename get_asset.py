8c47e3b5b452df860a933c1489b9bfe048a5d88b
Daniel Greaves <77550299+daniel-greaves@users.noreply.github.com>
2025-01-14 17:30:43 +0000

    Create get_asset.py

---
+++ get_asset.py
@@ -0,0 +1,25 @@
+from extras.scripts import *
+from django.utils.text import slugify
+from circuits.models import Provider, Circuit, ProviderNetwork, VirtualCircuit, VirtualCircuitTermination
+from dcim.models import Device, Interface
+from netbox_inventory.models import Asset
+
+import json
+
+class RunCommand(Script):
+
+    class Meta:
+        name = "Get ONT Asset"
+        description = "Get ONT Asset"
+
+        ont = ObjectVar(
+            model=Asset,
+            label="ONT",
+            description="ONT",
+            required=True
+        )
+
+    def run(self, data, commit):
+
+        self.log_info(f"ONT: {data['ont']}")
+        return ("Done")
