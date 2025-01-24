cc3c4ededcd29aa7694cb35b372d18c771556293
Daniel Greaves <77550299+daniel-greaves@users.noreply.github.com>
2025-01-24 00:04:11 +0000

    Update NewBranchScript.py

--- NewBranchScript.py
+++ NewBranchScript.py
@@ -15,111 +15,22 @@ class NewBranchScript(Script):
         description="Connection Reference of ONT to check",
         model=Device
     )
-    site_name = StringVar(
-        description="Name of the new site"
-    )
-    switch_count = IntegerVar(
-        description="Number of access switches to create"
-    )
-    switch_model = ObjectVar(
-        description="Access switch model",
-        model=DeviceType
-    )
-    router_count = IntegerVar(
-        description="Number of routers to create"
-    )
-    router_model = ObjectVar(
-        description="Router model",
-        model=DeviceType
-    )
-    ap_count = IntegerVar(
-        description="Number of APs to create"
-    )
-    ap_model = ObjectVar(
-        description="AP model",
-        model=DeviceType
-    )
-    server_count = IntegerVar(
-        description="Number of servers to create"
-    )
-    server_model = ObjectVar(
-        description="Server model",
-        model=DeviceType
-    )
 
     def run(self, data, commit):
+        device = data['device']
+        comments = data['comments']
+        kind = data['kind']
 
-        # Create the new site
-        site = Site(
-            name=data['site_name'],
-            slug=slugify(data['site_name']),
-            status=SiteStatusChoices.STATUS_PLANNED
-        )
-        site.save()
-        self.log_success(f"Created new site: {site}")
-
-        # Create access switches
-        switch_role = DeviceRole.objects.get(name='Access Switch')
-        for i in range(1, data['switch_count'] + 1):
-            switch = Device(
-                device_type=data['switch_model'],
-                name=f'{site.slug.upper()}-SW-{i}',
-                site=site,
-                status=DeviceStatusChoices.STATUS_PLANNED,
-                role=switch_role
+        if commit:
+            journal_entry = JournalEntry.objects.create(
+                assigned_object=device,
+                assigned_object_type=device.get_content_type(),
+                created=now(),
+                kind=kind,
+                comments=comments
             )
-            switch.save()
-            self.log_success(f"Created new switch: {switch}")
+            self.log_success(f"Journal entry successfully created for {device}")
+        else:
+            self.log_info(f"(Dry-run) Journal entry would be created for {device}")
 
-        # Create routers
-        router_role = DeviceRole.objects.get(name='WAN Router')
-        for i in range(1, data['router_count'] + 1):
-            router = Device(
-                device_type=data['router_model'],
-                name=f'{site.slug.upper()}-RTR-{i}',
-                site=site,
-                status=DeviceStatusChoices.STATUS_PLANNED,
-                role=router_role
-            )
-            router.save()
-            self.log_success(f"Created new router: {router}")
-
-        # Create APs
-        ap_role = DeviceRole.objects.get(name='Wireless AP')
-        for i in range(1, data['ap_count'] + 1):
-            ap = Device(
-                device_type=data['ap_model'],
-                name=f'{site.slug.upper()}-AP-{i}',
-                site=site,
-                status=DeviceStatusChoices.STATUS_PLANNED,
-                role=ap_role
-            )
-            ap.save()
-            self.log_success(f"Created new AP: {router}")
-        
-        # Create Servers
-        server_role = DeviceRole.objects.get(name='vSphere')
-        for i in range(1, data['server_count'] + 1):
-            server = Device(
-                device_type=data['server_model'],
-                name=f'{site.slug.upper()}-VSP-{i}',
-                site=site,
-                status=DeviceStatusChoices.STATUS_PLANNED,
-                role=server_role
-            )
-            server.save()
-            self.log_success(f"Created new server: {router}")
-
-        # Generate a CSV table of new devices
-        output = [
-            'name,make,model'
-        ]
-        for device in Device.objects.filter(site=site):
-            attrs = [
-                device.name,
-                device.device_type.manufacturer.name,
-                device.device_type.model
-            ]
-            output.append(','.join(attrs))
-
-        return '\n'.join(output)
+        return f"Journal entry created: {comments}"
