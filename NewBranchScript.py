2090a9ba4a7ba625bc7fa67f6570d067cc0fc825
Daniel Greaves <77550299+daniel-greaves@users.noreply.github.com>
2025-01-25 12:02:16 +0000

    Update NewBranchScript.py

--- NewBranchScript.py
+++ NewBranchScript.py
@@ -1,43 +1,24 @@
-from extras.scripts import *
+from dcim.models import Site, Device, DeviceType
 from django.utils.text import slugify
-
-from dcim.choices import DeviceStatusChoices, SiteStatusChoices
-from dcim.models import Device, DeviceRole, DeviceType, Site
-from netbox_inventory.models import Asset
+from extras.choices import JournalEntryKindChoices
+from extras.scripts import *
 from extras.models import JournalEntry
+from netbox_inventory.models import Asset
 from utilities.exceptions import AbortScript
 import json
 
-class NewBranchScript(Script):
 
+class heartbeat(Script):
+    
     class Meta:
-        name = "ONT Replacement"
-        description = "Replace an ONT  for a given FTTP Connection"
-
-    device = ObjectVar(
-        description="Connection Reference of ONT to check",
-        model=Device
-    )
-
-    new_ont = ObjectVar(
-        label = "New ONT",
-        description = "Serial Number of new ONT",
-        model=Asset
-    )
-
+        name = "heartbeat"
+        description = "Heartbeat"
+        
     def run(self, data, commit):
-        device = data['device']
-
-        entry = device.journal_entries.create()
-        entry.kind = "info"
-        entry.comments = "test journel entry"
-        entry.clean()
-        entry.save()
-
-        self.log_info("quicktest")
+        self.log_info("Heartbeat")
         
         return ("Done")
-
+        
 class quicktest2(Script):
         
     class Meta:
